30810f2ab0d394ad9074f5d94ddce2fa8cafb56e
Normano64 <per.bergqwist@gmail.com>
2015-11-13 16:28:41 +0100

    Fix for firefox for api calls

--- actions/index.js
+++ actions/index.js
@@ -12,17 +12,19 @@ export function addProduct(ean) {
   return dispatch => {
     dispatch(requestProduct(ean));
     return fetch(`http://dev.foocash.me/api/products/?code=${ean}`, {
-        headers: {
-          'Authorization': 'Token ###'
-        }
-      })
-      .then(response => response.json())
-      .then(data => {
-        if (data.length) {
-          dispatch(receiveProduct(data[0]))
-        }
-      });
-  }
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Token ###'
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.length) {
+        dispatch(receiveProduct(data[0]));
+      }
+    });
+  };
 };
 
 export function requestProduct(ean) {
@@ -30,33 +32,33 @@ export function requestProduct(ean) {
     type: REQUEST_PRODUCT,
     ean
   };
-}
+};
 
 export function receiveProduct(data) {
   return {
     type: ADD_PRODUCT,
     ...data
-  }
-}
+  };
+};
 
 export function increaseProductQty(count) {
   return {
     type: INCREASE_PRODUCT_QTY,
     count
   };
-}
+};
 
 export function selectProduct(ean) {
   return {
     type: SELECT_PRODUCT,
     ean
   };
-}
+};
 
 export function removeProduct() {
   return {
     type: REMOVE_PRODUCT
-  }
+  };
 };
 
 /* account - TODO: move to a separate module */
@@ -65,26 +67,28 @@ export function requestAccount(cardId) {
     type: REQUEST_ACCOUNT,
     cardId
   };
-}
+};
 
 export function receiveAccount(data) {
   return {
     type: LOGIN_ACCOUNT,
     data: data
-  }
-}
+  };
+};
 
 export function login(cardId) {
   return dispatch => {
     dispatch(requestAccount(cardId));
     return fetch(`http://dev.foocash.me/api/accounts/${cardId}/`, {
-        headers: {
-          'Authorization': 'Token ###'
-        }
-      })
-      .then(response => response.json())
-      .then(data => {
-        dispatch(receiveAccount(data))
-      });
-  }
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Token ###'
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      dispatch(receiveAccount(data));
+    });
+  };
 }
