5efa5bbce5847689b1d1ad42cfbe1ce666b7c6b3
Krzysztof Jagiello <me@kjagiello.com>
2015-11-15 14:07:40 +0100

    Cache products

--- actions/index.js
+++ actions/index.js
@@ -5,26 +5,120 @@ export const INCREASE_PRODUCT_QTY = 'INCREASE_PRODUCT_QTY';
 export const SELECT_PRODUCT = 'SELECT_PRODUCT';
 export const REMOVE_PRODUCT = 'REMOVE_PRODUCT';
 export const REQUEST_PRODUCT = 'REQUEST_PRODUCT';
+export const CHANGE_PAGE = 'CHANGE_PAGE';
 export const REQUEST_ACCOUNT = 'REQUEST_ACCOUNT';
 export const LOGIN_ACCOUNT = 'LOGIN_ACCOUNT';
 
-export function addProduct(ean) {
-  return dispatch => {
+class Cache {
+  constructor() {
+    this.cache = {};
+  }
+
+  hasKey(key, allowExpired=false) {
+    if (allowExpired)
+      return key in this.cache;
+    return key in this.cache && (new Date()) < this.cache[key].expires;
+  }
+
+  hasExpired(key) {
+    if (!(key in this.cache))
+      return null;
+    return (new Date()) >= this.cache[key].expires;
+  }
+
+  add(key, value, ttl) {
+    var expirationDate = new Date();
+    expirationDate.setSeconds(expirationDate.getSeconds() + ttl);
+
+    this.cache[key] = {
+      data: value,
+      expires: expirationDate
+    }
+  }
+
+  get(key, allowExpired=false) {
+    if (!allowExpired && this.hasKey(key))
+      return this.cache[key].data;
+    if (allowExpired && key in this.cache)
+      return this.cache[key].data;
+    return null;
+  }
+}
+
+class ProductFetcher {
+  constructor() {
+    this.cache = new Cache();
+    this.promises = {};
+  }
+
+  productWithEAN(dispatch, ean) {
+    if (this.cache.hasKey(ean)) {
+      var product = this.cache.get(ean);
+      dispatch(requestProduct(ean));
+      dispatch(receiveProduct(product));
+      return;
+    }
+    if (ean in this.promises) {
+      // fetching of the product in progress
+      return this.promises[ean];
+    }
+    if (this.cache.hasKey(ean, true)) {
+      // the product record has expired, but use it anyway
+      // and refresh it in background
+      dispatch(requestProduct(ean));
+      dispatch(receiveProduct(this.cache.get(ean, true)));
+
+      this.promises[ean] = fetch(
+        `http://dev.foocash.me/api/products/?code=${ean}`, {
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Token ###'
+          }
+        }
+      )
+      .then(response => response.json())
+      .then(data => {
+        if (data.length) {
+          this.cache.add(ean, data[0]);
+        }
+        delete this.promises[ean];
+      });
+
+      return this.promises[ean];
+    }
+
+    // product record not found in the cache, fetch it and dispatch
+    // appropriate actions
     dispatch(requestProduct(ean));
-    return fetch(`http://dev.foocash.me/api/products/?code=${ean}`, {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': 'Token ###'
+    this.promises[ean] = fetch(
+      `http://dev.foocash.me/api/products/?code=${ean}`, {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': 'Token ###'
+        }
       }
-    })
+    )
     .then(response => response.json())
     .then(data => {
       if (data.length) {
-        dispatch(receiveProduct(data[0]));
+        this.cache.add(ean, data[0], 5 * 60);
+        dispatch(receiveProduct(data[0]))
       }
+      delete this.promises[ean];
     });
-  };
+
+    return this.promises[ean];
+  }
+}
+
+var fetcher = new ProductFetcher();
+
+export function addProduct(ean) {
+  return dispatch => {
+    return fetcher.productWithEAN(dispatch, ean);
+  }
 };
 
 export function requestProduct(ean) {
